b56b8349c7f20a2997582283916cdbf36116a18b
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-07 23:04:10 +0530

    add Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+
 @NgModule({
   declarations: [
     AppComponent
