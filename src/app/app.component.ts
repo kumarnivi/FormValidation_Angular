c83a663ce258fb828aa4637d7c61c65f0f833dfd
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-07 20:25:25 +0530

    ReactiveForm_editTemplet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup,Validators } from '@angular/forms'
+import { min } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  integerRegex = /^\d+$/
+  emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
   title = 'reactive_form';
+
+
+  registerForm = new FormGroup ({
+    fname : new FormControl("", [Validators.required, Validators.maxLength(32)]),
+    lname : new FormControl("", [Validators.required, Validators.maxLength(32)]),
+    age : new FormControl("", [Validators.required, Validators.max(60), Validators.min(18), Validators.pattern(this.integerRegex)]),
+    mobile : new FormControl("", [Validators.required, Validators.maxLength(10), Validators.minLength(10)]),
+    email : new FormControl("", [Validators.required, Validators.maxLength(32), Validators.pattern(this.emailRegex)]),
+    password : new FormControl("", [Validators.required, Validators.maxLength(32), Validators.minLength(8)]),
+    conformPassword : new FormControl("", [Validators.required, Validators.maxLength(32), Validators.minLength(8)]),
+
+  })
 }
